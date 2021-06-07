398fa373c457faec57f4e06663541cd3151f5ec0
shizhaoheng <mi@123456>
2021-06-07 17:54:07 +0800

    反转单链表-递归解法

--- leetcode/src/main/java/leetcode/editor/cn/P206ReverseLinkedList.java
+++ leetcode/src/main/java/leetcode/editor/cn/P206ReverseLinkedList.java
@@ -72,6 +72,8 @@ public class P206ReverseLinkedList {
         printAll(headRev);
         System.out.println(node4);
         System.out.println(headRev);
+
+
     }
 
     public static void printAll(ListNode list) {
@@ -106,7 +108,8 @@ public class P206ReverseLinkedList {
      * }
      */
     class Solution {
-        public ListNode reverseList(ListNode head) {
+        //方法一：双指针迭代
+        public ListNode reverseList1(ListNode head) {
             ListNode prev = null, curr = head, nextTmp = null;
             while (curr != null) {
                 nextTmp = curr.next;
@@ -116,6 +119,26 @@ public class P206ReverseLinkedList {
             }
             return prev;
         }
+
+        //方法一：递归解法
+        public ListNode reverseList(ListNode head) {
+            //递归终止条件是当前为空，或者下一个节点为空
+            if(head==null || head.next==null) {
+                return head;
+            }
+            //这里的cur就是最后一个节点
+            ListNode cur = reverseList(head.next);
+            //这里请配合动画演示理解
+            //如果链表是 1->2->3->4->5，那么此时的cur就是5
+            //而head是4，head的下一个是5，下下一个是空
+            //所以head.next.next 就是5->4
+            head.next.next = head;
+            //防止链表循环，需要将head.next设置为空
+            head.next = null;
+            //每层递归函数都返回cur，也就是最后一个节点
+            return cur;
+        }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
