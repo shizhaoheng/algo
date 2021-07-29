a7f87a7eb3f0b7c3bf5cf257a08d84f655d7bf59
shizhaoheng <mi@123456>
2021-07-29 14:44:19 +0800

    环形链表

---
+++ leetcode/src/main/java/leetcode/editor/cn/ListNode.java
@@ -0,0 +1,22 @@
+package leetcode.editor.cn;
+
+/**
+ * @author : Shi Zhaoheng
+ * @date : 2021/7/29 14:00
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
